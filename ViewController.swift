b5768b9c1fd2128ea4a63e25e30c694344e3b643
claudiu <claudiu.garba@gmail.com>
2015-12-20 13:04:45 +0100

    yes

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  worldTrotter
+//
+//  Created by Girba Claudiu-ion on 12/20/15.
+//  Copyright © 2015 Girba Claudiu-ion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
